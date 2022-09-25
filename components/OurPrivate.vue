ff728ecb657605610d4366478b06b6459b311bf5
Mohamed Taki <m.taki.ed.mt@gmail.com>
2022-09-25 15:09:45 +0100

    Trip/Activity global component: Layout phase

---
+++ components/OurPrivate.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+.container
+  .content
+    h2.title Our Private Trips
+    .posts
+      Post(v-for="(post,i) in posts" :key="i" :post="post" :color="color")
+</template>
+<script>
+export default {
+  name: 'OurPrivate',
+  data() {
+    return {
+      posts: [
+        {
+          img: 'post.png',
+          title: 'Imperial Cities Tour 8 days',
+          description:
+            'Generally speaking, This tour remains vital since it is an actual opportunity to learn more and more about the countless features making Moroccan history along with its culture',
+          price: 850,
+          url: 'https://www.google.com/',
+        },
+        {
+          img: 'post.png',
+          title: 'Imperial Cities Tour 8 days',
+          description:
+            'Generally speaking, This tour remains vital since it is an actual opportunity to learn more and more about the countless features making Moroccan history along with its culture',
+          price: 850,
+          url: 'https://www.google.com/',
+        },
+        {
+          img: 'post.png',
+          title: 'Imperial Cities Tour 8 days',
+          description:
+            'Generally speaking, This tour remains vital since it is an actual opportunity to learn more and more about the countless features making Moroccan history along with its culture',
+          price: 850,
+          url: 'https://www.google.com/',
+        },
+        {
+          img: 'post.png',
+          title: 'Imperial Cities Tour 8 days',
+          description:
+            'Generally speaking, This tour remains vital since it is an actual opportunity to learn more and more about the countless features making Moroccan history along with its culture',
+          price: 850,
+          url: 'https://www.google.com/',
+        },
+        {
+          img: 'post.png',
+          title: 'Imperial Cities Tour 8 days',
+          description:
+            'Generally speaking, This tour remains vital since it is an actual opportunity to learn more and more about the countless features making Moroccan history along with its culture',
+          price: 850,
+          url: 'https://www.google.com/',
+        },
+        {
+          img: 'post.png',
+          title: 'Imperial Cities Tour 8 days',
+          description:
+            'Generally speaking, This tour remains vital since it is an actual opportunity to learn more and more about the countless features making Moroccan history along with its culture',
+          price: 850,
+          url: 'https://www.google.com/',
+        },
+      ],
+      color: {
+        text: 'text-heath',
+        btn: 'heath',
+      },
+    }
+  },
+}
+</script>
+<style scoped>
+.container {
+  @apply min-w-full px-2 flex justify-center bg-cotton;
+  @apply sm:px-6 lg:px-1
+}
+.container .content {
+  @apply w-full max-w-6xl mt-16 px-10 pb-24;
+}
+.title {
+  @apply text-4xl text-heath mb-9;
+  @apply xl:text-8xl lg:text-7xl sm:text-6xl;
+}
+.posts {
+  @apply grid gap-x-11 gap-y-14;
+  @apply lg:grid-cols-3 sm:grid-cols-2
+}
+</style>
---
+++ components/Post.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+.post
+  img.img(:src="post.img")
+  .content
+    h3.title(:class="color.text") {{post.title}}
+    p.description {{post.description}}
+    .info
+      .price-parent
+        span STARTING FROM
+        span.price ${{post.price}}
+      NuxtLink.ant-btn(:to="post.url" :class="color.btn") Learn More
+        iconsArrowRight
+  
+</template>
+
+<script>
+export default {
+  name: 'postComponent',
+  props: ['post', 'color'],
+}
+</script>
+
+<style scoped>
+.content{
+  @apply px-2 max-h-80 sm:max-w-sm;
+}
+.img {
+  @apply w-full mb-6;
+}
+.title {
+  @apply text-2xl mb-2;
+  @apply md:text-2xl sm:text-xl;
+}
+.description {
+  @apply text-xs text-stone mb-3;
+}
+.info {
+  @apply flex justify-between;
+}
+.price-parent {
+  @apply flex items-end gap-1;
+}
+.price-parent span:first-child{
+  @apply text-xs;
+}
+.price {
+  @apply text-xl font-medium;
+  @apply sm:text-2xl;
+}
+.ant-btn{
+  @apply w-max text-xs py-1 px-4 flex items-center text-albescent;
+  @apply sm:py-2 sm:px-6;
+}
+</style>
