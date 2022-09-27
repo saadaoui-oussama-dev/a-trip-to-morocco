063a0c9ac22130879a33510c17c053e3284794d9
Saadaoui Oussama <oussama.saadaoui.dev@gmail.com>
2022-09-27 12:44:45 +0100

    move code from middleware to index/asyncData

--- middleware/set-all-trips.js
+++
@@ -1,5 +0,0 @@
-export default async function ({ store }) {
-  let trips = await store.dispatch('landing/SET_PRIVATE_TRIPS')
-  trips = await store.dispatch('landing/SET_DAY_TRIPS')
-  trips = await store.dispatch('landing/SET_ACTIVITIES')
-}
